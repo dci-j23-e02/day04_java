0fa810197a3ddeb2269e9421190ad288fd4f4a12
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-10-09 11:59:45 +0200

    ScannerExperiments

--- src/ScannerExperiments.java
+++ src/ScannerExperiments.java
@@ -12,10 +12,26 @@ public class ScannerExperiments {
     System.out.println("Enter your name: ");
 
     // take the input from the keyboard
+    // String input
     String name = input.nextLine();
+
+    // Numerical input
+    System.out.println("Enter your age: ");
+    int age = input.nextInt();
+
+    System.out.println("Enter your salary: ");
+    double salary = input.nextDouble();
 
     // print the name
     System.out.println("My name is " + name);
+    // print the age
+    System.out.println("My age is " + age);
+    // print the salary
+    System.out.println("My salary is " + salary);
+
+
+
+
 
     // close the scanner
     input.close();
